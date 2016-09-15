0916f60e8e0f122bff5b59297feb38144a380cf7
zhangzewei <453646474@qq.com>
2016-09-15 20:10:38 +0000

    feat: 完善各个页面

--- js/index.js
+++ js/index.js
@@ -41,3 +41,30 @@ $(document).ready(function() {
       responsiveSlides: true,
   });
 });
+
+(function() {
+
+  function init() {
+    var speed = 300,
+      easing = mina.backout;
+
+    [].slice.call ( document.querySelectorAll( '#grid > a' ) ).forEach( function( el ) {
+      var s = Snap( el.querySelector( 'svg' ) ), path = s.select( 'path' ),
+        pathConfig = {
+          from : path.attr( 'd' ),
+          to : el.getAttribute( 'data-path-hover' )
+        };
+
+      el.addEventListener( 'mouseenter', function() {
+        path.animate( { 'path' : pathConfig.to }, speed, easing );
+      } );
+
+      el.addEventListener( 'mouseleave', function() {
+        path.animate( { 'path' : pathConfig.from }, speed, easing );
+      } );
+    } );
+  }
+
+  init();
+
+})();
